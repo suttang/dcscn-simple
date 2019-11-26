cac1909f5607671ec77768b33e9880f2c657376f
suttang <suttang@gmail.com>
2019-11-26 19:01:33 +0900

    evaluate中

--- train.py
+++ train.py
@@ -1,3 +1,5 @@
+import os
+
 import numpy as np
 
 from model import Dcscn
@@ -5,7 +7,41 @@ from model import Dcscn
 
 def main():
     model = Dcscn()
+
+    test_files = get_test_files("set5")
+    psnr, ssim = get_metrics(model.test_files)
+
     model.train()
+
+    import pdb
+
+    pdb.set_trace()
+
+    test_files = "set5"
+    # psnr, ssim = model.evaluate()
+
+
+def get_metrics(model, files):
+    psnrs = 0
+    ssims = 0
+
+    for file in files:
+        psnr, ssim = model.evaluate(file)
+        psnrs += psnr
+        ssims += ssim
+    psnr /= len(files)
+    ssim = ssims / len(files)
+
+    return psnr, ssim
+
+
+def get_test_files(dataset_name):
+    dataset_dir = os.path.join(os.getcwd(), "data", dataset_name)
+    return [
+        dataset_dir + file
+        for file in os.listdir(dataset_dir)
+        if os.path.isfile(os.path.join(dataset_dir, file)) and not file.startswith(".")
+    ]
 
 
 if __name__ == "__main__":
